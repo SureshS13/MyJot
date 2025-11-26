f440e284e94f538cc3452684a8682a60f5e013f7
SachinSureshBusiness <83098929+SureshS13@users.noreply.github.com>
2025-11-25 20:51:16 -0600

    Summary of Changes:
    - Adjusted the baseStyles.css CSS stylesheet to specify the font type in the font family as "sans-serif".
    
    - Added an additional utility method to the SharedAppUtilities.js JS script, and adjusted the export syntax to export the functions at the bottom of the script.

--- js/site/shared/SharedAppUtilities.js
+++ js/site/shared/SharedAppUtilities.js
@@ -1,9 +1,33 @@
 /**
+* Deletes all the children of the inputted HTML elements from the DOM.
+* @param {HTMLElement[]} [elements=[]] An Array containing HTMLElements.
+*/
+function deleteAllChildren(elements = []) {
+    if (Array.isArray(elements)) {
+        for (const element of elements) {
+            element.replaceChildren();
+        }
+    }
+}
+
+/**
+* @typedef {Object} HTMLElementOptions
+* @property {string} [type="p"] The type of the HTML element (e.g., "div", "span").
+* @property {string} [text=""] The text content of the HTML element.
+* @property {HTMLElement[]} [children=[]] An array of child elements to append to the created element.
+* @property {Array.<{name: string, value: string}>} [attributes=[]] An array of objects, each containing `name` and `value` properties for each of the attributes to set on the element.
+* @property {string[]} [classes=[]] An array of CSS classes to add to the created element.
+* @property {string} [id=""] The ID to assign to the created element.
+* @property {Object} [additionalOptions={}] Additional options for creating the HTML element.
+* @property {boolean} [additionalOptions.placeChildrenBeforeText=false] Whether to place children before the text content.
+*/
+
+/**
 * Creates an HTML element with specified attributes and children.
 * @param {HTMLElementOptions} options Options for creating the HTML element.
 * @returns {HTMLElement} The created HTML element.
 */
-export function createHTMLElement({ type = "p", text = "", children = [], attributes = [], classes = [], id = "", additionalOptions = { placeChildrenBeforeText: false } }) {
+function createHTMLElement({ type = "p", text = "", children = [], attributes = [], classes = [], id = "", additionalOptions = { placeChildrenBeforeText: false } }) {
     let element = document.createElement(type);
     let elementText = document.createTextNode(text);
 
@@ -37,3 +61,5 @@ export function createHTMLElement({ type = "p", text = "", children = [], attrib
 
     return element;
 }
+
+export { deleteAllChildren, createHTMLElement }
